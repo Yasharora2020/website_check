4e6f931144725eefb0871b48447dfa92cddb6bc9
Yash <yashvardhanarora@gmail.com>
2023-03-02 20:47:11 +1100

    added some code

--- monitor.py
+++ monitor.py
@@ -1,3 +1,25 @@
+import requests
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+url = "https://www.dnypractice.com.au"
+EMAIL_ADDRESS = os.getenv("EMAIL_USER")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASS")
+
+r = requests.get(url, timeout=10)
+
+if r.status_code !=200:
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.ehlo()
+        connection.starttls()
+        connection.ehlo()
+        connection.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        connection.sendmail(EMAIL_ADDRESS,EMAIL_ADDRESS, msg="Subject:Website Down")
+
 
 
 
